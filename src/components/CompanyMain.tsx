88f456ed0b70f3be4e61d5170181fe4d3bc2643c
Willy <willech@hotmail.com>
2024-09-24 13:18:23 +0200

    Make pagination actually work

--- src/components/CompanyMain.tsx
+++ src/components/CompanyMain.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import type { Response } from '../hooks/useCompanies'
 import useCompanies from '../hooks/useCompanies';
+import { DefaultPagination } from './ui/Pagination';
 import { SortableTable } from './ui/SortableTable';
 
 const NUM_PER_PAGE = 10;
 
 const CompanyMain = () => {
     
-    
+    const [page, setPage] = useState(1)
+
     const allCompanies = useCompanies();
 
-    const companies = allCompanies.isSuccess ? (allCompanies.data as Response).enterprises.slice(0, NUM_PER_PAGE) : []
+    const startIndex = page*NUM_PER_PAGE - 10
 
+    const companies = allCompanies.isSuccess ? (allCompanies.data as Response).enterprises.slice(startIndex, page*NUM_PER_PAGE) : []
+    
     return (
         <div className='flex flex-col gap-6 items-center'>
             <SortableTable companies={companies}/>
+            <DefaultPagination active={page} setActive={setPage} />
         </div>
             
         
