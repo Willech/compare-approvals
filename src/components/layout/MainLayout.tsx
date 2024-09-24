2140a69f49b4d0294eceb8c9eb31d04ba5eee56d
Willy <willech@hotmail.com>
2024-09-24 13:28:40 +0200

    adjust layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,8 +5,8 @@ interface MainLayoutProps {
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex overflow-x-scroll">
-      <div className="w-full max-w-4xl mx-auto my-5 p-4">
-        <div className="flex flex-col items-center space-y-4">
+      <div className="my-5">
+        <div className="flex flex-col">
           {children}
         </div>
       </div>
